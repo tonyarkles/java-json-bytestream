3f701606f796ffadcf69647cf219598540d4761f
Tony Arkles <t.arkles@gmail.com>
2012-02-21 17:14:06 -0600

    beginnings of a unit test suite

---
+++ streaming_json/src/test/java/com/peakxp/streaming_json/StreamingJsonParserTest.java
@@ -0,0 +1,28 @@
+package com.peakxp.streaming_json;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class StreamingJsonParserTest 
+    extends TestCase
+{
+    public void testBlankObjectsOnInitialization() {
+	StreamingJsonParser sjp = new StreamingJsonParser();
+	List res = sjp.getParsed();
+	assertEquals( 0, res.size() );
+    }
+
+    public void testSimpleObjectReturned() {
+	StreamingJsonParser sjp = new StreamingJsonParser();
+	sjp.parseBytes("{}".getBytes());
+	List res = sjp.getParsed();
+	assertEquals( 1, res.size());
+    }
+
+}
