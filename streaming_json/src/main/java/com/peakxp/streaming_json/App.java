d85a898807d83e320cc1facdb248c3faedf78b8f
Tony Arkles <t.arkles@gmail.com>
2012-02-21 16:26:53 -0600

    initial commit of bare maven project

---
+++ streaming_json/src/main/java/com/peakxp/streaming_json/App.java
@@ -0,0 +1,13 @@
+package com.peakxp.streaming_json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
