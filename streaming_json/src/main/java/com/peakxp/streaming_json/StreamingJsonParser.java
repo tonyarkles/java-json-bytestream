e89df8d6bfb667ad2b18d397823535b760e4fcc8
Tony Arkles <t.arkles@gmail.com>
2012-02-21 17:07:34 -0600

    stub implementation

---
+++ streaming_json/src/main/java/com/peakxp/streaming_json/StreamingJsonParser.java
@@ -0,0 +1,13 @@
+package com.peakxp.streaming_json;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class StreamingJsonParser {
+    void parseBytes(byte[] input) {
+    }
+
+    List<Object> getParsed() {
+	return new ArrayList();
+    }
+}
