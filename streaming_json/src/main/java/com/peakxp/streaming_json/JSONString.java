e3f93b7ae9dfdffcf09a67e578fd205cec733251
Tony Arkles <t.arkles@gmail.com>
2012-02-21 22:54:47 -0600

    passing preliminary jsonstring tests

--- streaming_json/src/main/java/com/peakxp/streaming_json/JSONString.java
+++ streaming_json/src/main/java/com/peakxp/streaming_json/JSONString.java
@@ -1,8 +1,23 @@
 package com.peakxp.streaming_json;
 
 class JSONString extends JSONElement {
+    private StringBuilder sb;
+    private boolean started;
+    private boolean done;
 
+    public JSONString() {
+	this.sb = new StringBuilder();
+	this.started = false;
+	this.done = false;
+    }
     public boolean consume(char c) {
+	if (!this.started && (c == '"')) {
+	    this.started = true;
+	} else if (!this.done && (c == '"')) {
+	    this.done = true;
+	} else {
+	    sb.append(c);
+	}
 	return true;
     }
 
@@ -12,5 +27,9 @@ class JSONString extends JSONElement {
 
     public boolean isCompleted() {
 	return false;
+    }
+
+    public String getString() {
+	return sb.toString();
     }
 }
