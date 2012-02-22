1df8d1239475afeb9765041e1be5ddff0d4f871f
Tony Arkles <t.arkles@gmail.com>
2012-02-21 22:55:42 -0600

    missed the actual test file last time

---
+++ streaming_json/src/test/java/com/peakxp/streaming_json/JSONStringTest.java
@@ -0,0 +1,37 @@
+package com.peakxp.streaming_json;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class JSONStringTest
+    extends TestCase
+{
+    JSONString jst;
+    
+    public void setupWithString(String s) {
+	jst = new JSONString();
+	jst.consume('"');
+	for (int i = 0; i < s.length(); i++) {
+	    jst.consume(s.charAt(i));
+	}
+	jst.consume('"');
+    }
+
+    public void testEmptyString() {
+	setupWithString("");
+	assertEquals( "", jst.getString() );
+	assertTrue( jst.isCompleted() );
+    }
+
+    public void testWithOneCharacter() {
+	setupWithString("a");
+	assertEquals( "a", jst.getString() );
+	assertTrue( jst.isCompleted() );
+    }
+}
